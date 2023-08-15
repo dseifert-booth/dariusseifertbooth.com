d3798d699c609a00bcc6c3c382f573e1774d72eb
Darius <dariusasb@gmail.com>
2023-08-14 20:10:57 -0600

    new components

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+      <div className="Header">
+        <ul>
+            <a href="#">About Me</a>
+            <a href="#">DSB</a>
+            <a href="#">Contact Me</a>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default Header;
