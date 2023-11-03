8a1a4dba79203a94ff2d626b5563bae9c4792b3e
Darius <dariusasb@gmail.com>
2023-11-02 23:14:48 -0600

    starting to implement routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,37 @@
 import '../css/Header.css';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
+import About from './About';
+import InnerBody from './InnerBody';
+import Contact from './Contact';
 
 function Header() {
     return (
-      <div className="Header">
-        <ul>
-            <a href="#">About Me</a>
-            <a href="#">DSB</a>
-            <a href="#">Contact Me</a>
-        </ul>
-      </div>
+        <Router>
+            <div className="Header">
+                <ul>
+                    <Link to="/about">About Me</Link>
+                    <Link href="/">DSB</Link>
+                    <Link href="/contact">Contact Me</Link>
+                </ul>
+
+                <Routes>
+                    <Route path="/about">
+                        <About/>
+                    </Route>
+                    <Route path="/">
+                        <InnerBody/>
+                    </Route>
+                    <Route path="/contact">
+                        <Contact/>
+                    </Route>
+                </Routes>
+            </div>
+      </Router>
     );
   }
   
